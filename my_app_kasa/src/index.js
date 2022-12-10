c47137b8cbc755d4830ef8428270ff93415970de
cindy <r.cindy67@gmail.com>
2022-12-10 10:52:18 +0100

    supression fichier inutile+ creation index/app

--- my_app_kasa/src/index.js
+++ my_app_kasa/src/index.js
@@ -1,15 +1,26 @@
+import "./index.css"
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { BrowserRouter } from 'react-router-dom'; // Importation du Routeur
 import App from './App';
+import { useEffect } from 'react';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
+
+//On donne un titre à l'onglet
+function useTitle(title) {
+    useEffect(() => {
+      document.title = `Kasa | ${title}`;
+    });
+  }
+  
+  export default useTitle;
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
